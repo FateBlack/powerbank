1f332c078e229f8d1c59845525305dc4defab515
千夜 <1342746626@qq.com>
2019-03-19 11:18:45 +0800

    Initial commit

---
+++ src/main/java/com/lz/power/PowerbankApplication.java
@@ -0,0 +1,13 @@
+package com.lz.power;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerbankApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/lz/power/controller/LoginController.java
@@ -0,0 +1,8 @@
+package com.lz.power.controller;
+
+/**
+ * Created by 白 on 2019/3/19.
+ */
+public class LoginController {
+
+}
